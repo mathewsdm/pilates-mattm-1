cb8b16cd28ce87f06b6eb314a2da84c42129ea86
mathewsdm <mathewsdmoreira@gmail.com>
2024-08-27 12:01:48 -0700

    Update Home.c1dmp.js

---
+++ src/pages/Home.c1dmp.js
@@ -0,0 +1,8 @@
+import { Permissions, webMethod } from "wix-web-module";
+
+export const multiply = webMethod(
+  Permissions.Anyone, 
+  (factor1, factor2) => { 
+    return factor1 * factor2 
+  }
+);
